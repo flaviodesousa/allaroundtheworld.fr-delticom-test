f6a723a6f619d70ee1755fbe68870328f9763922
Flávio de Sousa <fl@viodesousa.com>
2013-06-27 16:26:57 -0300

    Importing csv into db

--- importer.pl
+++ importer.pl
@@ -14,13 +14,28 @@ our @EXPECTED_COLUMN_HEADERS = qw(order_date customer_id customer_first_name cus
 
 
 
+sub verify_if_source_file_not_yet_processed {
+	my ($schema, $fullname) = @_;
+	my $source_rs = $schema->resultset('Source');
+	my $source_data = {full_path_name => $fullname};
+	if ($source_rs->find($source_data)) {
+		warn "File \"$fullname\" already imported. Ignoring.\n";
+		return undef;
+	}
+	return $source_rs->create($source_data);
+}
+
+
 sub do_import {
+	my $schema = shift;
 	my $csv = Text::CSV->new( { binary => 1, allow_whitespace => 1 } )
 		or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
 	foreach my $filename (@ARGV) {
-		my $fullname = abs_path($filename);
-		say "Fullname=$fullname";
+
+		my $source = verify_if_source_file_not_yet_processed($schema, abs_path($filename));
+		next unless $source;
+
 		open my $fh, "<:encoding(utf8)", $filename
 			or die "$filename: $!";
 		
@@ -34,7 +49,31 @@ sub do_import {
 		$csv->bind_columns( \@{$row_ref}{@col_names} );
 
 		while ( $csv->getline( $fh ) ) {
-			say join '/', %$row_ref;
+			my $customer = AATW::Schema::Result::Customer->find_or_create($schema,
+				{
+				alternate_id => $row_ref->{customer_id},
+				first_name => $row_ref->{customer_first_name},
+				last_name => $row_ref->{customer_last_name}
+				});
+			my ($date, $time) = split ' ', $row_ref->{order_date};
+			my $order = AATW::Schema::Result::Order->find_or_create($schema,
+				$customer,
+				{					
+				number => $row_ref->{order_number},
+				date => $date,
+				time => $time
+				});
+			my $item = AATW::Schema::Result::Item->find_or_create($schema,
+				{
+				name => $row_ref->{item_name},
+				manufacturer => $row_ref->{item_manufacturer}
+				});
+			my $price = $row_ref->{item_price}; $price =~ s/[^\d.]//g;
+			$item->item_prices->create({
+				price => $price,
+				order_id => $order->id,
+				source_id => $source->id
+				});
 		}
 
 		close $fh;
@@ -45,5 +84,5 @@ sub do_import {
 
 
 
-AATW::Schema::Deploy->deploy_db();
-do_import();
+my $schema = AATW::Schema::Deploy->deploy_db();
+do_import($schema);
