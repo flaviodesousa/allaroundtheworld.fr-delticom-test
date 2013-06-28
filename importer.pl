ea4b004ad3d4c045cb2a8fc5adbeaf91620568cd
Flávio de Sousa <fl@viodesousa.com>
2013-06-28 14:18:50 -0300

    reformatted by perltidy

--- importer.pl
+++ importer.pl
@@ -11,97 +11,102 @@ use Text::CSV;
 use AATW::Schema::Deploy;
 use AATW::Schema;
 
-our ($schema, @EXPECTED_COLUMN_HEADERS);
+our ( $schema, @EXPECTED_COLUMN_HEADERS );
 
-@EXPECTED_COLUMN_HEADERS = qw(order_date customer_id customer_first_name customer_last_name order_number item_name item_manufacturer item_price);
-
-
+@EXPECTED_COLUMN_HEADERS =
+  qw(order_date customer_id customer_first_name customer_last_name order_number item_name item_manufacturer item_price);
 
 sub input_file_already_imported {
-	my ($fully_qualified_file_name) = @_;
-	my $source_rs = $schema->resultset('Source');
-	my $source_data = {full_path_name => $fully_qualified_file_name};
-	if ($source_rs->find($source_data)) {
-		warn "File \"$fully_qualified_file_name\" already imported. Ignoring.\n";
-		return 1;
-	}
-	return;
+    my ($fully_qualified_file_name) = @_;
+    my $source_rs = $schema->resultset('Source');
+    my $source_data = { full_path_name => $fully_qualified_file_name };
+    if ( $source_rs->find($source_data) ) {
+        warn
+          "File \"$fully_qualified_file_name\" already imported. Ignoring.\n";
+        return 1;
+    }
+    return;
 }
-
-
 
 sub do_import {
-	my $csv = Text::CSV->new( { binary => 1, allow_whitespace => 1 } )
-		or croak "Cannot use CSV: ".Text::CSV->error_diag ();
+    my $csv = Text::CSV->new( { binary => 1, allow_whitespace => 1 } )
+      or croak "Cannot use CSV: " . Text::CSV->error_diag();
 
-	for my $filename (@ARGV) {
+    for my $filename (@ARGV) {
 
-		my $fully_qualified_file_name = abs_path($filename);
+        my $fully_qualified_file_name = abs_path($filename);
 
-		next if input_file_already_imported($fully_qualified_file_name);
+        next if input_file_already_imported($fully_qualified_file_name);
 
-		open my $fh, "<:encoding(utf8)", $fully_qualified_file_name
-			or croak "$fully_qualified_file_name: $!";
-		
-		# is the 1st line a valid header?
-		my @col_names = map { s/ /_/g; $_ } @{ $csv->getline( $fh ) };
-		if (join(',', @EXPECTED_COLUMN_HEADERS) ne join(',', @col_names)) {
-			warn "$fully_qualified_file_name: malformed header, expected column headers not present.\n";
-			next;
-		}
+        open my $fh, "<:encoding(utf8)", $fully_qualified_file_name
+          or croak "$fully_qualified_file_name: $!";
 
-		my $source = $schema->resultset('Source')->find_or_create(
-			{full_path_name => $fully_qualified_file_name},
-			{key => 'unique_sources'});
+        # is the 1st line a valid header?
+        my @col_names = map { s/ /_/g; $_ } @{ $csv->getline($fh) };
+        if ( join( ',', @EXPECTED_COLUMN_HEADERS ) ne join( ',', @col_names ) )
+        {
+            warn
+"$fully_qualified_file_name: malformed header, expected column headers not present.\n";
+            next;
+        }
 
-		my $row_ref = {};
-		$csv->bind_columns( \@{$row_ref}{@col_names} );
+        my $source =
+          $schema->resultset('Source')
+          ->find_or_create( { full_path_name => $fully_qualified_file_name },
+            { key => 'unique_sources' } );
 
-		while ( $csv->getline( $fh ) ) {
-			my $customer = $schema->resultset('Customer')->find_or_create(
-				{
-					alternate_id => $row_ref->{customer_id},
-					first_name => $row_ref->{customer_first_name},
-					last_name => $row_ref->{customer_last_name}
-				},
-				{
-					key => 'unique_customers'
-				});
-			my ($date, $time) = split ' ', $row_ref->{order_date};
-			my $order = $customer->orders->find_or_create(
-				{					
-					number => $row_ref->{order_number},
-					date => $date,
-					time => $time
-				},
-				{
-					key => 'unique_orders'
-				});
-			my $item = $schema->resultset('Item')->find_or_create(
-				{
-					name => $row_ref->{item_name},
-					manufacturer => $row_ref->{item_manufacturer}
-				},
-				{
-					key => 'unique_items'
-				});
-			my $price = $row_ref->{item_price}; $price =~ s/[^\d.]//g;
-			$item->item_prices->create({
-				price => $price,
-				order_id => $order->id,
-				source_id => $source->id
-				});
-		}
+        my $row_ref = {};
+        $csv->bind_columns( \@{$row_ref}{@col_names} );
 
-		close $fh;
+        while ( $csv->getline($fh) ) {
+            my $customer = $schema->resultset('Customer')->find_or_create(
+                {
+                    alternate_id => $row_ref->{customer_id},
+                    first_name   => $row_ref->{customer_first_name},
+                    last_name    => $row_ref->{customer_last_name}
+                },
+                {
+                    key => 'unique_customers'
+                }
+            );
+            my ( $date, $time ) = split ' ', $row_ref->{order_date};
+            my $order = $customer->orders->find_or_create(
+                {
+                    number => $row_ref->{order_number},
+                    date   => $date,
+                    time   => $time
+                },
+                {
+                    key => 'unique_orders'
+                }
+            );
+            my $item = $schema->resultset('Item')->find_or_create(
+                {
+                    name         => $row_ref->{item_name},
+                    manufacturer => $row_ref->{item_manufacturer}
+                },
+                {
+                    key => 'unique_items'
+                }
+            );
+            my $price = $row_ref->{item_price};
+            $price =~ s/[^\d.]//g;
+            $item->item_prices->create(
+                {
+                    price     => $price,
+                    order_id  => $order->id,
+                    source_id => $source->id
+                }
+            );
+        }
 
-		$csv->eof or $csv->error_diag();
-	}
+        close $fh;
 
-	return;
+        $csv->eof or $csv->error_diag();
+    }
+
+    return;
 }
-
-
 
 $schema = AATW::Schema::Deploy->deploy_db();
 
--- reporter.pl
+++ reporter.pl
@@ -8,108 +8,103 @@ use AATW::Schema::Deploy;
 use AATW::Schema;
 use AATW::Report::Text;
 
-our ($schema, $reporter);
-
-
+our ( $schema, $reporter );
 
 sub order_details {
-	my $order_id = shift;
-	my $order_items = $schema->resultset('ItemPrice')->search(
-		{
-			order_id => $order_id
-		});
-	while (my $item = $order_items->next) {
-		$reporter->item_price_line(
-			$item->item->name,
-			$item->price);
-	}
+    my $order_id    = shift;
+    my $order_items = $schema->resultset('ItemPrice')->search(
+        {
+            order_id => $order_id
+        }
+    );
+    while ( my $item = $order_items->next ) {
+        $reporter->item_price_line( $item->item->name, $item->price );
+    }
 }
-
-
 
 sub customer_totals_by_order {
-	my ($day, $customer_id) = @_;
-	my $by_orders = $schema->resultset('Order')->search(
-		{
-			date => $day,
-			customer_id => $customer_id
-		},
-		{
-			join     => [ 'item_prices' ],
-			select   => [
-						'me.id',
-						'me.number',
-						{ sum => 'item_prices.price'}
-						],
-			as       => [ qw(order_id order_number total) ],
-			group_by => [ 'me.id' ],
-			order_by => [ 'me.number' ]
-		});
-	while (my $order = $by_orders->next) {
-		$reporter->order_header(
-			$order->get_column('order_number'),
-			$order->get_column('total'));
-		order_details($order->get_column('order_id'));
-	}
+    my ( $day, $customer_id ) = @_;
+
+    # totals by orders placed by a given customer on a given day
+    my $by_orders = $schema->resultset('Order')->search(
+        {
+            date        => $day,
+            customer_id => $customer_id
+        },
+        {
+            join   => ['item_prices'],
+            select => [ 'me.id', 'me.number', { sum => 'item_prices.price' } ],
+            as     => [qw(order_id order_number total)],
+            group_by => ['me.id'],
+            order_by => ['me.number']
+        }
+    );
+    while ( my $order = $by_orders->next ) {
+        $reporter->order_header( $order->get_column('order_number'),
+            $order->get_column('total') );
+        order_details( $order->get_column('order_id') );
+    }
 }
-
-
 
 sub day_totals_by_customer {
-	my $day = shift;
-	my $by_customers = $schema->resultset('Order')->search(
-		{ date => $day },
-		{
-			join     => [ 'customer', 'item_prices' ],
-			select   => [
-						'customer.id',
-						'customer.alternate_id', 
-						'customer.first_name', 
-						'customer.last_name', 
-						{ sum => 'item_prices.price'} ],
-			as       => [ qw(customer_id alternate_id first_name last_name total) ],
-			group_by => [ 'customer.id' ],
-			order_by => [ 'customer.last_name', 'customer.first_name ' ]
-		});
-	while (my $customer = $by_customers->next) {
-		$reporter->customer_header(
-			$customer->get_column('first_name'),
-			$customer->get_column('last_name'),
-			$customer->get_column('customer_id'),
-			$customer->get_column('total'));
-		customer_totals_by_order($day, $customer->get_column('customer_id'));
-	}
-}
+    my $day          = shift;
 
+    # totals by customer for a given day
+    my $by_customers = $schema->resultset('Order')->search(
+        { date => $day },
+        {
+            join   => [ 'customer', 'item_prices' ],
+            select => [
+                'customer.id',         'customer.alternate_id',
+                'customer.first_name', 'customer.last_name',
+                { sum => 'item_prices.price' }
+            ],
+            as => [qw(customer_id alternate_id first_name last_name total)],
+            group_by => ['customer.id'],
+            order_by => [ 'customer.last_name', 'customer.first_name ' ]
+        }
+    );
+    while ( my $customer = $by_customers->next ) {
+        $reporter->customer_header(
+            $customer->get_column('first_name'),
+            $customer->get_column('last_name'),
+            $customer->get_column('customer_id'),
+            $customer->get_column('total')
+        );
+        customer_totals_by_order( $day, $customer->get_column('customer_id') );
+    }
+}
 
 sub do_report {
-	# totals by day
-	my $days = $schema->resultset('Order')->search(
-		{},
-		{
-			join     => [ 'item_prices' ],
-			select   => [ 
-						'date', 
-						{ sum => 'item_prices.price' },
-						{ count => 'distinct me.id' } ],
-			as       => [ 'date', 'total', 'order_count' ],
-			group_by => [ 'date' ],
-			order_by => [ 'date desc' ]
-		});
 
-	while (my $day = $days->next) {
-		$reporter->day_header(
-					$day->date, 
-					$day->get_column('order_count'), 
-					$day->get_column('total'));
-		#say "Day= ",$day->date," ",$day->get_column('total'),' Orders:',$day->get_column('order_count');
-		day_totals_by_customer($day->date);
-	}
+    # totals by day
+    my $days = $schema->resultset('Order')->search(
+        {},
+        {
+            join   => ['item_prices'],
+            select => [
+                'date',
+                { sum   => 'item_prices.price' },
+                { count => 'distinct me.id' }
+            ],
+            as       => [ 'date', 'total', 'order_count' ],
+            group_by => ['date'],
+            order_by => ['date desc']
+        }
+    );
+
+    while ( my $day = $days->next ) {
+        $reporter->day_header(
+            $day->date,
+            $day->get_column('order_count'),
+            $day->get_column('total')
+        );
+
+        day_totals_by_customer( $day->date );
+    }
 }
 
-
-
-$schema = AATW::Schema::Deploy->deploy_db();
+$schema   = AATW::Schema::Deploy->deploy_db();
 $reporter = AATW::Report::Text->new();
 
 do_report;
