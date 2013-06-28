fd303c46fe45b3305204a5a939e84c780113e039
Flávio de Sousa <fl@viodesousa.com>
2013-06-28 14:07:44 -0300

    perltidy error fixed

--- importer.pl
+++ importer.pl
@@ -3,6 +3,7 @@
 use 5.014;
 use strict;
 use warnings;
+use Carp;
 
 use Cwd 'abs_path';
 use Text::CSV;
@@ -10,40 +11,43 @@ use Text::CSV;
 use AATW::Schema::Deploy;
 use AATW::Schema;
 
-our @EXPECTED_COLUMN_HEADERS = qw(order_date customer_id customer_first_name customer_last_name order_number item_name item_manufacturer item_price);
+our ($schema, @EXPECTED_COLUMN_HEADERS);
+
+@EXPECTED_COLUMN_HEADERS = qw(order_date customer_id customer_first_name customer_last_name order_number item_name item_manufacturer item_price);
 
 
 
 sub input_file_already_imported {
-	my ($schema, $fully_qualified_file_name) = @_;
+	my ($fully_qualified_file_name) = @_;
 	my $source_rs = $schema->resultset('Source');
 	my $source_data = {full_path_name => $fully_qualified_file_name};
 	if ($source_rs->find($source_data)) {
 		warn "File \"$fully_qualified_file_name\" already imported. Ignoring.\n";
 		return 1;
 	}
-	return undef;
+	return;
 }
 
 
-sub do_import {
-	my $schema = shift;
-	my $csv = Text::CSV->new( { binary => 1, allow_whitespace => 1 } )
-		or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
-	foreach my $filename (@ARGV) {
+sub do_import {
+	my $csv = Text::CSV->new( { binary => 1, allow_whitespace => 1 } )
+		or croak "Cannot use CSV: ".Text::CSV->error_diag ();
+
+	for my $filename (@ARGV) {
 
 		my $fully_qualified_file_name = abs_path($filename);
 
-		next if input_file_already_imported($schema, $fully_qualified_file_name);
+		next if input_file_already_imported($fully_qualified_file_name);
 
 		open my $fh, "<:encoding(utf8)", $fully_qualified_file_name
-			or die "$fully_qualified_file_name: $!";
+			or croak "$fully_qualified_file_name: $!";
 		
 		# is the 1st line a valid header?
 		my @col_names = map { s/ /_/g; $_ } @{ $csv->getline( $fh ) };
 		if (join(',', @EXPECTED_COLUMN_HEADERS) ne join(',', @col_names)) {
-			die "$fully_qualified_file_name: malformed header, expected column headers not present.\n";
+			warn "$fully_qualified_file_name: malformed header, expected column headers not present.\n";
+			next;
 		}
 
 		my $source = $schema->resultset('Source')->find_or_create(
@@ -93,9 +97,12 @@ sub do_import {
 
 		$csv->eof or $csv->error_diag();
 	}
+
+	return;
 }
 
 
 
-my $schema = AATW::Schema::Deploy->deploy_db();
-do_import($schema);
+$schema = AATW::Schema::Deploy->deploy_db();
+
+do_import;
