384a0be3ee20f37de870cc57d6c13c2caf80a77f
Flávio de Sousa <fl@viodesousa.com>
2013-06-28 18:07:05 -0300

    tar maker working

--- mktar.sh
+++ mktar.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 
 cd ~/dev/aatw
+rm -rf /tmp/aatw -v
 [ -d /tmp/aatw ] || mkdir /tmp/aatw
 cp * /tmp/aatw -Rv
 cd /tmp/aatw && (
-	rm aatw.sqlite3
-	rm *.sublime-*
-	rm mktar.sh
-	rm sample.rpt
-	mv specs/order.csv .
-	rm specs/*
-	rmdir specs
-	./importer.pl order.csv
-	./reporter.pl > sample.rpt
+	rm aatw.sqlite3 -v
+	rm *.sublime-* -v
+	rm mktar.sh -v
+	rm sample.rpt -v
+	mv specs/orders.csv . -v
+	rm specs/* -v
+	rmdir specs -v
+	./importer.pl ./orders.csv
+	./reporter.pl > ./sample.rpt
 	cd ..
 	tar Jcvf aatw.xz aatw
 	ls -l aatw.xz
