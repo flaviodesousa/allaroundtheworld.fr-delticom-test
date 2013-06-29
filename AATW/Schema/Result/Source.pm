21c5a8bead3edde8f66424f74e2547eaf356431b
Flávio de Sousa <fl@viodesousa.com>
2013-06-29 00:32:47 -0300

    perltidy ho!

--- AATW/Schema/Result/Source.pm
+++ AATW/Schema/Result/Source.pm
@@ -10,7 +10,8 @@ our $VERSION = q(0.0.1);
 __PACKAGE__->table('sources');
 __PACKAGE__->add_columns(qw/ id full_path_name /);
 __PACKAGE__->set_primary_key('id');
-__PACKAGE__->add_unique_constraint('unique_sources' => [qw(full_path_name)]);
-__PACKAGE__->has_many('item_prices', 'AATW::Schema::Result::ItemPrice', 'source_id');
+__PACKAGE__->add_unique_constraint( 'unique_sources' => [qw(full_path_name)] );
+__PACKAGE__->has_many( 'item_prices', 'AATW::Schema::Result::ItemPrice',
+    'source_id' );
 
 1;
